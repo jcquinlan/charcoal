8e0ed50addf4440678145b8efaca35a361dd3918
James Quinlan <jcquinlan.dev@gmail.com>
2017-04-23 10:12:10 -0400

    Rewrite noncharcoal data to destination file

--- src/charcoal.js
+++ src/charcoal.js
@@ -1,20 +1,8 @@
 const process = require('process');
-
 const Charcoal = require('./charcoalClass');
-
-/*
-// JS Variable Regex test.
-http://pythex.org/?regex=(%3F%3Aconst%7Clet%7Cvar)%7B1%7D%20%5Cw%2B%20%3D%20(%3F%3A%27%7C%22)*((%3F%3A%23*)%5B%5Cw%5C(%5C)%2C%5D%2B)(%3F%3A%27%7C%22)*%3B*&test_string=let%20breakpoint%20%3D%201990%0Aconst%20test%20%3D%20%27%2300ff45%27%0Alet%20anything_2%20%3D%20%27%2300ff45%27%3B%0Avar%20blahBlah%20%3D%20%27%2300ff45%27%3B%0Aconst%20blahBlah%20%3D%20%22%2300ff45%22%3B%0Aconst%20blah%20Blah%20%3D%20%22rgb(0%2C255%2C255)%22%3B%0Aconst%20blahBlah%20%3D%20%22rgb(0%2C255%2C255)%22%3B%0Aconst%20blahBlah%20%3D%20%22yellow%22%3B&ignorecase=0&multiline=0&dotall=0&verbose=0
-*/
-
-/*
-// SCSS Variable Regex test
-http://pythex.org/?regex=%5C%24(%5Cw%2B)%3A%20%23*(%5B%5Cw%5C(%5C)%2C%5D%2B)%3B*&test_string=%24varOne%3A%20%23333%3B%0A%24var_two%3A%20blue%0A%24varThree%3A%20rgb(0%2C255%2C255)%3B%0A%24breakpoint%3A%201991%3B%0A&ignorecase=0&multiline=0&dotall=0&verbose=0
-*/
 
 const srcFile = process.argv[2];
 const destFile = process.argv[3];
-
 const main = new Charcoal(srcFile, destFile);
 
 main.run();
--- src/charcoalClass.js
+++ src/charcoalClass.js
@@ -5,36 +5,85 @@ const promiseWrap = require('./utility');
 
 class Charcoal {
     constructor(srcFile, destFile){
-        this.srcFile    = srcFile;
-        this.destFile   = destFile;
-        this.jsRegex    = /(?:const|let|var){1} (\w+) = (?:'|")*((?:#*)[\w\(\),]+)(?:'|")*;*/;
-        this.scssRegex  = /\$(\w+): #*([\w\(\),]+);*/
+        this.srcFile       = srcFile;
+        this.destFile      = destFile;
+        this.srcFileData   = null;
+        this.destFileData  = null;
+        this.jsRegex       = /(?:const|let|var){1} (\w+) = (?:'|")*((?:#*)[\w\(\),]+)(?:'|")*;*/i;
+        this.scssRegex     = /\$(\w+): #*([\w\(\),]+);*/i;
+        this.charcoalRegex = /\/+\*+\s*Charcoal Variables\s*\*+\/+/i;
     }
 
+
     run() {
-        const exists = promiseWrap(this.checkDestExists.bind(this))
-            .then(res => promiseWrap(this.readSourceFile.bind(this)))
-            .then(variables => promiseWrap(this.writeVariablesToDestFile.bind(this), variables))
-            .then((done) => this.handleComplete(done))
+        const exists = promiseWrap(this.checkDestinationExists.bind(this))
+            .then(()     => promiseWrap(this.readDestinationFile.bind(this)))
+            .then(()     => promiseWrap(this.readSourceFile.bind(this)))
+            .then(()     => promiseWrap(this.writeToDestinationFile.bind(this)))
+            .then(done   => this.handleComplete(done))
             .catch(error => this.handleError(error));
     }
 
-    checkDestExists(resolve, reject) {
+
+    checkDestinationExists(resolve, reject) {
         fs.access(this.destFile, fs.constants.F_OK, (error) => {
             if(error) reject(error);
             resolve();
         })
     }
 
+
     extractVariablesFromData(data){
         const lines = data.split('\n');
 
        return lines.reduce((array, line) => {
-            const variable = line.match(this.jsRegex, 'i');
+            const variable = line.match(this.jsRegex);
             if(variable) array.push(variable);
             return array;
         }, []);
     }
+
+
+    // Keep track of all the current lines written in the destination file.
+    // These will need to be rewritten when JS variables are updated.
+    extractNonCharcoalData(data){
+        const lines = data.split('\n');
+        let destinationFileLines = [];
+        let foundLine = false;
+
+        for(let index = 0; index < lines.length; index++) {
+            const line = lines[index];
+
+            if(foundLine) continue;
+            if(line === '' && (index === lines.length - 1 || index === 0)) continue;
+
+            const charcoalLine = line.match(this.charcoalRegex);
+
+            if(charcoalLine) {
+                destinationFileLines.push(line);
+                foundLine = true;
+            } else {
+                destinationFileLines.push(line);
+            }
+        };
+
+        return destinationFileLines;
+    }
+
+
+    readDestinationFile(resolve, reject) {
+        // Load contents of the variable JS file
+        const file = fs.readFile(this.destFile, 'utf8', (error, data) => {
+            // If there is an error, reject it.
+            if(error) { 
+                reject(error);
+            } else {
+                this.destFileData = this.extractNonCharcoalData(data);
+                resolve();
+            }
+        });
+    }
+
 
     readSourceFile(resolve, reject) {
         // Load contents of the variable JS file
@@ -43,39 +92,46 @@ class Charcoal {
             if(error) { 
                 reject(error);
             } else {
-                resolve(this.extractVariablesFromData(data));
+                this.srcFileData = this.extractVariablesFromData(data);
+                resolve();
             }
         });
     }
 
-    writeVariablesToDestFile(resolve, reject, variables){
-        const { destFile } = this;
-        const logger = fs.createWriteStream(destFile)
+
+    writeToDestinationFile(resolve, reject){
+        const logger = fs.createWriteStream(this.destFile)
 
         // Write each variable passed in to the destFile, and format them as SCSS variables
-        variables.forEach(variable => {
-            logger.write(this.generateSCSSVariableString(variable));
+        this.destFileData.forEach((line, index) => {
+            logger.write(`${ line }\n`);
+        })
+
+        this.srcFileData.forEach((variable, index) => {
+            logger.write(`${ this.generateSCSSVariableString(variable) }${ index === this.srcFileData.length - 1 ? '' : '\n' }`);
         });
 
-        logger.end();
-        resolve(`Wrote ${ variables.length } variables to ${ this.destFile }`);
+        resolve();
     }
+
 
     generateSCSSVariableString(regexCapture){
         const name = regexCapture[1];
         const value = regexCapture[2];
 
-        return `$${ name }: ${ value };\n`
+        return `$${ name }: ${ value };`
     }
+
 
     handleError(error){
         console.log(`${ colorIt('Charcoal Error:').alizarin() }`)
         console.log(`${ colorIt(error).alizarinBg() }`);
     }
 
-    handleComplete(message){
+
+    handleComplete(){
         console.log(`${ colorIt('Charcoal copy completed.').greenBg() }`);
-        console.log(`${ colorIt(message).greenBg() }`);
+        console.log(`${ colorIt(`Wrote ${ this.srcFileData.length } variables to ${ this.destFile }`).greenBg() }`);
     }
 }
 
