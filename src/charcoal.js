86cd48df7258d099a613aad5d7c6905bf26c0c4f
James Quinlan <jamesquinlan@dhcp-wifi-8021x-155-41-17-8.bu.edu>
2017-04-30 10:25:03 -0400

    Add basic argument validation

--- src/charcoal.js
+++ src/charcoal.js
@@ -1,9 +1,14 @@
 const process = require('process');
 const Charcoal = require('./class');
 
-const srcFile = process.argv[2];
-const destFile = process.argv[3];
+if(process.argv.length > 4){
+    console.error('Charcoal takes only 2 arguments: a source JS file, and a destination SCSS file.')
+    console.error(`Unknown argument: ${ process.argv[4] }`);
+} else {
+    const srcFile = process.argv[2];
+    const destFile = process.argv[3];
 
-const main = new Charcoal(srcFile, destFile);
+    const main = new Charcoal(srcFile, destFile);
 
-main.run();
+    main.run();
+}
